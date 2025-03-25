eb23207a50c7f619d227523f8a1286f660821382
suleiman <suleimannwr@gmail.com>
2025-03-25 16:12:58 +0300

    done

--- src/ui/ResumeButton.jsx
+++ src/ui/ResumeButton.jsx
@@ -1,8 +1,12 @@
 function ResumeButton() {
+	// Hardcode the absolute path
+	const pdfUrl = 'https://Suleiman010.github.io/baraa-portfolio/Resume.pdf';
+
 	return (
 		<a
-			href="/Albaraa Almashwakhi CV.pdf"
+			href={pdfUrl}
 			target="_blank"
+			rel="noopener noreferrer"
 			className="btn-hover-effect bg-transparent border-2 border-white hover:border-white text-white font-bold py-4 px-8 rounded-lg transition shadow-lg pulse"
 		>
 			View Resume
